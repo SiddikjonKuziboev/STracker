29fbc8ccab25f5b598dff9717c3451866430bc86
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-05-07 11:43:49 +0500

    Initial Commit

---
+++ STracker/STrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  STrackerApp.swift
+//  STracker
+//
+//  Created by Siddikjon Kuziboev on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct STrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
